dd358bb1e9a0f529953c96dcce2171fb4cc508c4
Vincent Daranyi <vincent@daranyi.com>
2015-03-25 19:10:22 -0400

    user creation working

--- routes/index.js
+++ routes/index.js
@@ -32,13 +32,32 @@ router.post('/getCommenters', function(req, res, next) {
 });
 
 router.get('/:user/newsfeed', function(req, res, next){
-  User.findOne({id: req.params.user}, function(err, user){
-    console.log(err, user);
-    Item.find({by: {$in: user.following}}).sort([['id','descending']]).exec(function(err, items){
-      res.send(items);
-    });
+  var username = req.params.user,
+      newsfeed;
+  User.findOne({id: username}, function(err, user){
+    if (!user) {
+      // move into different file
+      console.log(username, typeof username);
+      User.create({id: username, following: ['tptacek','patio11','jacquesm','ColinWright','edw519','fogus','tokenadult','danso','shawndumas','jgrahamc']}, function(err, user){
+        console.log('USERNO:',err, user);
+        newsfeed = getNewsfeed(user);
+        console.log('NEWS:',newsfeed);
+      });
+    } else {
+      console.log('USERYES:',user);
+      newsfeed = getNewsfeed(user);
+      console.log('NEWS:',newsfeed);
+    }
   });
+  
+  function getNewsfeed(user) {
+    Item.find({by: {$in: user.following}}).sort([['id','descending']]).exec(function(err, newsfeed){
+      res.send(newsfeed);
+    });
+  } 
+
 });
+
 
 router.get('/:user/notifications', function(req, res, next){
   
