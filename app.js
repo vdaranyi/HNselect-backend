b66f3687abea0a551050ec3c1b71159bd4f9e4a1
Vincent Daranyi <vincent@daranyi.com>
2015-04-03 16:34:27 -0400

    trying

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error: ', err.message);
+  // res.send('error: ', err.message);
+  res.status(status).send(body);
 });
 
 module.exports = app;
