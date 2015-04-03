ce7c4af57d9d08caed2a8ae41b9a7933c128b0a4
Vincent <crsmnd@users.noreply.github.com>
2015-04-03 15:39:09 -0400

    Delete development.js

--- env/development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "DATABASE_URI": "mongodb://localhost/hn-select",
-  	 twitter: { consumerKey: '2wxtcPKYc7ABKQKm7Dk4wHLRe',
-				consumerSecret: 'SXfA4cBT7kZkkyxjVSgUCfHKcK6b6EjEhLPrdtKhVPqpJTj0zd',
-				callbackUrl: 'http://127.0.0.1:3000/user/twitter/connected'
-	}
-};
